e9a6ac44d29538466e9f3870b08a4a7f037e9cd2
성근제 <bluemen02@naver.com>
2020-07-15 18:22:14 +0900

    로그인 토큰과 로그인 상태 유지 구현

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Vue cookies
+import VueCookies from 'vue-cookies'
+Vue.use(VueCookies)
+Vue.config.productionTip = false
+// axios
+import axios from 'axios'
+Vue.prototype.$axios = axios
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- frontend/src/page/user/Login.vue
+++ frontend/src/page/user/Login.vue
@@ -64,9 +64,9 @@
                     if (res.status == 200) {
                         console.log(res)
                         document.cookie = `accessToken=${res.data.data.accessToken}`
-                        axios.default.headers.common['x-access-token'] = res.data.data.accessToken
-                        this.$route.push({'name':'main'})
-                    } else { alert('로그인 실패') }
+                        this.$emit('submit-login',res.data.object)
+                        this.$router.push({'name':'main'})
+                    } else { alert('로그인 실패!!!') }
                 })
                 .catch(err => {
                     alert('로그인 실패!')
