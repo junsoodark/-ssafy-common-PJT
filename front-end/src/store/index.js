d050ae662ec35faa43796bde7e272ceabd83cd76
성근제 <bluemen02@naver.com>
2020-07-21 15:43:25 +0900

    로그인과 vuex 중간단계

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+// import Axios from 'axios'
+// import VueCookies from 'vue-cookies'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    authToken:  'asdf'
   },
   mutations: {
+    // Login (state,{cooky}) {
+    //   state.authToken = cooky
+    // },
+    // Logout (state) {
+    //   state.authToken = null
+    // }
   },
   actions: {
+    Login (state,{email,password}) {
+      // Axios.get('http://localhost:3000/accounts/login',{
+      //   params: {
+      //     email: email,
+      //     password: password
+      //   }
+      //   .then(res => {
+      //     this.commit('Login',res.data.object)
+      //     this.$cookies.set('auth-token',res.data.object)
+      //     this.IsLoggedIn = true
+      //   })
+      //   .catch(err => {console.log(err)})
+      // })
+      alert(email,password)
+    }
   },
   modules: {
+  },
+  getters: {
+    IsLoggedIn: state => !!state.authToken
   }
 })
