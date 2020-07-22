2f8916c51b852268e23ee5badcb90e064e3c2c10
성근제 <bluemen02@naver.com>
2020-07-22 18:22:18 +0900

    머지 후 오류사항 수정

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,8 +3,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import VueCookies from 'vue-cookies'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -16,7 +14,7 @@ Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
 // bootstrap
-import BootstrapVue from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
