25fa1af43d616d350c2f5518eedc78f6879f733d
성근제 <bluemen02@naver.com>
2020-07-22 16:17:49 +0900

    메인화면 케로우셀 삽입 & 휠 이벤트 구현

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,7 +1,65 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="top-carousel" ref="toporto">
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        caption="First slide"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <template v-slot:img>
+          <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="https://picsum.photos/1024/480/?image=55"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide>
+
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
+          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+    </b-carousel>
+
+    <p class="mt-4">
+      Slide #: {{ slide }}<br>
+      Sliding: {{ sliding }}
+    </p>
+    </div>
+    <div id="bot-div" ref="porto">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
   </div>
 </template>
 
@@ -13,6 +71,29 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  methods: {
+    scrollMeTo(refName) {
+    var element = this.$refs[refName];
+    var top = element.offsetTop;
+    window.focus()
+    window.scrollTo({top:top,left:0,behavior:'smooth'});
+    }
+  },
+  mounted () {
+    const topWindow = document.querySelector('#top-carousel')
+    let scrollMeTo = this.scrollMeTo
+    topWindow.addEventListener('mousewheel',function(event){
+      if (event.deltaY>0) {
+        setTimeout(function(){scrollMeTo('porto')},0)
+      } 
+    })
+    const botWindow = document.querySelector('#bot-div')
+    botWindow.addEventListener('mousewheel',function(event){
+      if (event.deltaY<0) {
+        setTimeout(function(){scrollMeTo('toporto')},0)
+      } 
+    })
   }
 }
 </script>
