76568e60654ff5c74cbbdeb6e2446ab338b7d3d2
이영인 <qhduddlsl@naver.com>
2020-07-23 23:03:58 +0900

    날짜 범위 패키지 설치

--- front-end/src/views/team/CreateTeam.vue
+++ front-end/src/views/team/CreateTeam.vue
@@ -12,7 +12,12 @@
       </b-form-group>
 
       <b-form-group id="input-group-2" label="분야:" label-for="input-2">
-        <b-form-select id="input-2" v-model="form.field" :options="fields" required></b-form-select>
+        <b-form-select
+          id="input-2"
+          v-model="form.field"
+          :options="fields"
+          required
+        ></b-form-select>
       </b-form-group>
 
       <b-form-group id="input-group-3" label="방식:" label-for="input-3">
@@ -21,14 +26,26 @@
       </b-form-group>
 
       <b-form-group id="input-group-4" label="지역:" label-for="input-4">
-        <b-form-select id="input-4" v-model="form.area" :options="areas" required></b-form-select>
+        <b-form-select
+          id="input-4"
+          v-model="form.area"
+          :options="areas"
+          required
+        ></b-form-select>
       </b-form-group>
 
       <b-form-group id="input-group-5" label="스터디 일정:" label-for="input-5">
         <b-button id="input-5">매월</b-button>
         <b-button id="input-5">매주</b-button>
         <b-button id="input-5">추후협의</b-button>
-        <b-form-spinbutton id="input-5" v-model="count" :option="counts" min="1" max="7" required></b-form-spinbutton>
+        <b-form-spinbutton
+          id="input-5"
+          v-model="count"
+          :option="counts"
+          min="1"
+          max="7"
+          required
+        ></b-form-spinbutton>
       </b-form-group>
 
       <b-form-group id="input-group-6" label="요일:" label-for="input-6">
@@ -45,6 +62,10 @@
         <b-button id="input-7">추후협의</b-button>
       </b-form-group>
 
+      <b-form-froup id="input-group-8" label="스터디 기간:" laebl-for="input-8">
+        <v-md-date-range-picker></v-md-date-range-picker>
+      </b-form-froup>
+
       <b-button type="submit" variant="primary">Submit</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
     </b-form>
@@ -60,21 +81,21 @@ export default {
         field: null,
         area: null,
         count: null,
-        checked: []
+        checked: [],
       },
       fields: [
         { text: "원하는 분야를 선택해주세요", value: null },
         "공기업",
-        "사기업"
+        "사기업",
       ],
       areas: [
         { text: "원하는 지역을 선택해주세요", value: null },
         "강남구",
         "강북구",
-        "종로구"
+        "종로구",
       ],
       counts: [{ text: "횟수", value: null }],
-      show: true
+      show: true,
     };
   },
   methods: {
@@ -94,10 +115,9 @@ export default {
       this.$nextTick(() => {
         this.show = true;
       });
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
