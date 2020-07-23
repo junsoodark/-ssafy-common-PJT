5927c4e49a4d5e8196dd436c6d8e6cbf322623b1
박준수 <junsoodark@naver.com>
2020-07-23 16:27:50 +0900

    Add getter and setter

--- back-end/src/main/java/com/web/blog/model/auth/Verify.java
+++ back-end/src/main/java/com/web/blog/model/auth/Verify.java
@@ -17,4 +17,21 @@ public class Verify {
 	@Id
 	private String email;
 	private String code;
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
 }
