4bbdd0e155c9ab02e3ef04f6fec2e5128ea78842
boo <wjdtn7823@naver.com>
2020-07-23 20:24:45 +0900

    address 엔티티 변경

--- back-end/src/main/java/com/web/blog/model/address/Address.java
+++ back-end/src/main/java/com/web/blog/model/address/Address.java
@@ -1,11 +1,15 @@
 package com.web.blog.model.address;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Address")
 public class Address {
+	@Id @GeneratedValue
+	private String id;
 	
 	private String si;
 	private String gu;
