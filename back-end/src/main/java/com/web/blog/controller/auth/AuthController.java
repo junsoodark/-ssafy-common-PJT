9b35c3cbc94fd4761e8246322b8697dd732f6395
sangyeol <ten_ability@daum.net>
2020-07-23 22:50:43 +0900

    Remove unnecessary import

--- back-end/src/main/java/com/web/blog/controller/auth/AuthController.java
+++ back-end/src/main/java/com/web/blog/controller/auth/AuthController.java
@@ -4,11 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.web.blog.model.auth.Mail;
 import com.web.blog.service.auth.MailService;
 import com.web.blog.service.auth.VerifyService;
 
@@ -23,7 +21,7 @@ public class AuthController {
 	private MailService mailService;
 	
 	@PostMapping("/verify")
-	@ApiOperation(value="이메일을 입력받아 사용중인 이메일인지  확인하고, 사용중이지 않다면 인증 메일을 전송합니다.")
+	@ApiOperation(value="이메일을 입력받아 사용중인 이메일인지 확인하고, 사용중이지 않다면 인증 메일을 전송합니다.")
 	public ResponseEntity sendVerifyMail(@RequestParam final String email) {
 		if(verifyService.isDuplicated(email)) return new ResponseEntity("이미 사용중인 이메일 입니다.", HttpStatus.CONFLICT);
 		
