a1b9433539e954427adce546c1f258416bd09edf
sangyeol kim <ten_ability@daum.net>
2020-07-24 10:56:20 +0900

    Add study service

--- back-end/src/main/java/com/web/blog/dao/study/StudyDao.java
+++ back-end/src/main/java/com/web/blog/dao/study/StudyDao.java
@@ -1,20 +1,11 @@
-package com.web.blog.dao.study;
-
-import java.util.List;
-
-import com.web.blog.model.study.Study;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudyDao extends JpaRepository<Study, String> {
-    Study getStudyById(int id);
-
-    Study getStudyByTitle(String title);
-
-    void delete(Study study);
-
-    Study save(Study study);
-
-    List<Study>  findAll();
-
-}
+//package com.web.blog.dao.study;
+//
+//import java.util.Optional;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import com.web.blog.model.study.Study;
+//
+//public interface StudyDao extends JpaRepository<Study, String> {
+//	Optional<Study> findStudyByStudyId(final int study_id);
+//}
--- back-end/src/main/java/com/web/blog/service/study/StudyServiceImpl.java
+++ back-end/src/main/java/com/web/blog/service/study/StudyServiceImpl.java
@@ -1,45 +1,22 @@
-package com.web.blog.service.study;
-
-import java.util.List;
-
-import com.web.blog.dao.study.StudyDao;
-import com.web.blog.model.study.Study;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-public class StudyServiceImpl implements StudyServiece {
-
-    @Autowired
-    StudyDao studyDao;
-
-    @Override
-    public void update(Study study) {
-        studyDao.save(study);
-    }
-
-    @Override
-    public void delete(Study study) {
-        studyDao.delete(study);
-    }
-
-    @Override
-    public void create(Study study) {
-        studyDao.save(study);
-    }
-
-    @Override
-    public Study search(int study_id) {
-        return studyDao.getStudyById(study_id);
-    }
-
-    @Override
-    public Study search(String title) {
-        return studyDao.getStudyByTitle(title);
-    }
-
-    @Override
-    public List<Study> searchAll() {
-        return studyDao.findAll();
-    }
-
-}
+//package com.web.blog.service.study;
+//
+//import java.util.Optional;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import com.web.blog.dao.study.StudyDao;
+//import com.web.blog.model.study.Study;
+//
+//@Service
+//public class StudyServiceImpl implements StudyService{
+//	@Autowired
+//	StudyDao studyDao;
+//
+//	@Override
+//	public Study findStudyByStudyId(int study_id) {
+//		Optional<Study> studyOpt = studyDao.findStudyByStudyId(study_id);
+//		return studyOpt.isPresent() ? studyOpt.get() : null;
+////		return null;
+//	}
+//}
