c464769703d80d00440bc742c4d03544330c3059
성근제 <sungguenja@gmail.com>
2020-07-29 00:54:47 +0900

    allow cors

--- back-end/src/main/java/com/web/blog/config/WebConfig.java
+++ back-end/src/main/java/com/web/blog/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins("http://localhost:8080")
+				.allowedOrigins("http://localhost:8080","http://i3a607.p.ssafy.io/")
 				.allowedMethods("GET", "POST", "PUT", "DELETE");
 	}
 }
