0a3c2d6799f3aeb917f954b697bd4a47fbc4f872
sangyeol <ten_ability@daum.net>
2020-07-29 14:05:26 +0900

    Add convertor for response entity

--- back-end/src/main/java/com/web/blog/service/user/UserService.java
+++ back-end/src/main/java/com/web/blog/service/user/UserService.java
@@ -1,4 +1,4 @@
-package com.web.blog.service;
+package com.web.blog.service.user;
 
 import java.util.Map;
 
@@ -7,9 +7,11 @@ import org.springframework.validation.Errors;
 import com.web.blog.model.user.User;
 
 public interface UserService {
-	public Map<String, Object> findUserByEmail(final String email);
-	public Map<String, String> getErrorMessage(final Errors errors);
+	public User findUserByEmail(final String email);
+	public Map<String, Object> User2Map(final User user);
 	public boolean create(final User user);
 	public boolean update(final User user);
 	public boolean delete(final String email);
+	
+	public Map<String, String> getErrorMessage(final Errors errors);
 }
