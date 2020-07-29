49043d345efcb3fbc22c099e4760adb6554e9722
sangyeol <ten_ability@daum.net>
2020-07-29 14:10:12 +0900

    Implements CR for study

---
+++ back-end/src/main/java/com/web/blog/service/address/AddressServiceImpl.java
@@ -0,0 +1,21 @@
+package com.web.blog.service.address;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.web.blog.dao.address.AddressDao;
+import com.web.blog.model.address.Address;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+	@Autowired
+	private AddressDao addrDao;
+	
+	@Override
+	public Address findAddressBySiAndGu(final String si, final String gu) {
+		Optional<Address> addrOpt = addrDao.findBySiAndGu(si, gu);
+		return addrOpt.isPresent() ? addrOpt.get() : null;
+	}
+}
--- back-end/src/main/java/com/web/blog/service/study/StudyService.java
+++ back-end/src/main/java/com/web/blog/service/study/StudyService.java
@@ -1,7 +1,13 @@
 package com.web.blog.service.study;
 
+import java.util.List;
+import java.util.Map;
+
 import com.web.blog.model.study.Study;
 
 public interface StudyService {
-	public Study findStudyByStudyId(final int study_id);
+	public Study findStudyByStudyId(final int studyId);
+	public Map<String, Object> Study2Map(final Study study);
+	public List<Map<String, Object>> findAllStudies();
+	public void create(final Study study);
 }
