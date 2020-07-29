fe0a16475b0dca165ee1aeb7db59f27f73abd77c
sangyeol <ten_ability@daum.net>
2020-07-29 14:08:41 +0900

    Add repository annotation

--- back-end/src/main/java/com/web/blog/dao/user/UserDao.java
+++ back-end/src/main/java/com/web/blog/dao/user/UserDao.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 
 import com.web.blog.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserDao extends JpaRepository<User, String> {
 	Optional<User> findUserByEmail(String email);
 	Optional<User> findUserByEmailAndPassword(String email, String password);
