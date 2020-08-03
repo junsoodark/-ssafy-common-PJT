1edf1a659786c6e4e13eda240cea1c77ce116401
이영인 <qhduddlsl@naver.com>
2020-08-03 15:51:53 +0900

    axios get 요청으로 시 data 받아오기

--- front-end/src/views/team/CreateTeam.vue
+++ front-end/src/views/team/CreateTeam.vue
@@ -20,7 +20,8 @@
       </b-form-group>-->
 
       <b-form-group id="input-group-4" label="지역:" label-for="input-4">
-        <b-form-select id="input-4" v-model="form.siArea" :options="siAreas" required></b-form-select>
+        <b-button @click="getSi"></b-button>
+        <b-form-select id="input-4" v-model="form.si" :options="siAreas" required></b-form-select>
         <b-form-select id="input-4" v-model="form.guArea" :options="guAreas" required></b-form-select>
       </b-form-group>
 
@@ -90,7 +91,7 @@ export default {
         studyname: "",
         field: null,
         contact: null,
-        siArea: null,
+        si: null,
         guArea: null,
         schedule: null,
         count: null,
@@ -107,7 +108,7 @@ export default {
         "공기업",
         "사기업",
       ],
-      siAreas: [{ text: "원하는 지역을 선택해주세요", value: null }, "서울시"],
+      siAreas: [{ text: "원하는 지역을 선택해주세요", value: null }],
       guAreas: [
         { text: "원하는 지역을 선택해주세요", value: null },
         "강남구",
@@ -211,6 +212,16 @@ export default {
       this.$nextTick(() => {
         this.show = true;
       });
+    },
+    getSi() {
+      Axios.get(`${API_URL}address/`)
+        .then((res) => {
+          this.form.siAreas = res.data;
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
   },
 };
