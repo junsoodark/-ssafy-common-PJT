a990bf16393447b2dae964369c9b10460d3ddcd6
boo <wjdtn7823@naver.com>
2020-08-11 12:35:15 +0900

    스터디 가입승인 개발완료(TODO : 가입승인 관리자 권한 개발)

---
+++ back-end/src/main/java/com/web/blog/dao/study/StudyApprovalDao.java
@@ -0,0 +1,14 @@
+package com.web.blog.dao.study;
+
+import java.util.Set;
+
+import com.web.blog.model.study.Study;
+import com.web.blog.model.user.User;
+
+public interface StudyApprovalDao {
+	public boolean apply(Study study, User member);
+	public boolean disapply(Study study, User member);
+	public boolean isExistMemberApproval(Study study, User member);
+	public Set<User> getMemberApproval(Study study);
+	
+}
