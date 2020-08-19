59b0aa72d263882cf5f42f5976d6bb7cb66f22f1
boo <wjdtn7823@naver.com>
2020-08-20 00:04:48 +0900

    메일 전송 비동기화로 변경해서 성능 향상

--- back-end/src/main/java/com/web/blog/BackEndApplication.java
+++ back-end/src/main/java/com/web/blog/BackEndApplication.java
@@ -2,7 +2,8 @@ package com.web.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableAsync;
+@EnableAsync
 @SpringBootApplication
 public class BackEndApplication {
 
